d22619df99ddb51237ad0814101520318e9483b9
ljeandin <lucie.jeandin@gmail.com>
2025-03-01 12:04:23 +0100

    Revert scroll snapping, fix images

--- src/components/testimonials/Testimonials.vue
+++ src/components/testimonials/Testimonials.vue
@@ -1,19 +1,22 @@
 <script setup>
     const testimonials = [
         {
-            picture: "mathias",
+            picture:
+                "/public/images/testimonials/mathias.jpg",
             name: "Mathias",
             company: "Caféine Web",
             review: "Lucie s'est intégrée au projet avec aisance, faisant preuve d'efficacité, d'aisance technique, de réactivité et d'une grande précision dans le design et l'intégration des projets, ce fut un plaisir de travailler ensemble.",
         },
         {
-            picture: "alexandre",
+            picture:
+                "/public/images/testimonials/alexandre.jpg",
             name: "Alexandre",
             company: "Backmarket",
             review: "Je recommande vivement Lucie comme développeuse front end. Au cours de notre collaboration de deux ans, elle a toujours fait preuve d'une approche proactive, axée sur la recherche de solutions. Lucie sait s'adapter au changement, elle est rigoureuse dans l'exécution.",
         },
         {
-            picture: "frederic",
+            picture:
+                "/public/images/testimonials/frederic.jpg",
             name: "Frédéric",
             company: "SINGULART",
             review: "Techniquement compétente et extrêmement douée en communication, Lucie a constamment apporté des solutions créatives et des implémentations pertinentes à nos projets. Son enthousiasme et sa volonté d'apprendre sont contagieux, apportant une énergie positive et dynamisante à toute l'équipe.",
@@ -30,11 +33,7 @@
         >
             <img
                 class="testimonial__image"
-                :src="
-                    '/public/images/testimonials/' +
-                    testimonial.picture +
-                    '.jpg'
-                "
+                :src="testimonial.picture"
                 alt=""
             />
             <h2 class="testimonial__reviewer discrete">
