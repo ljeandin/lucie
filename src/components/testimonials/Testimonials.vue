883957e8fee61230795e052a658a5ebffec0cc75
ljeandin <lucie.jeandin@gmail.com>
2025-03-01 11:53:39 +0100

    FIll testimonials, snap testimonials to top of the page

--- src/components/testimonials/Testimonials.vue
+++ src/components/testimonials/Testimonials.vue
@@ -1,22 +1,22 @@
 <script setup>
     const testimonials = [
         {
-            picture: "alex",
-            name: "Alex",
-            company: "ApolloNet",
-            review: "Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quaeab illo inventore veritatis et quasi architectobeatae vitae dicta sunt explicabo.",
+            picture: "mathias",
+            name: "Mathias",
+            company: "Caféine Web",
+            review: "Lucie s'est intégrée au projet avec aisance, faisant preuve d'efficacité, d'aisance technique, de réactivité et d'une grande précision dans le design et l'intégration des projets, ce fut un plaisir de travailler ensemble.",
         },
         {
-            picture: "benjamin",
-            name: "Benjamin",
+            picture: "alexandre",
+            name: "Alexandre",
+            company: "Backmarket",
+            review: "Je recommande vivement Lucie comme développeuse front end. Au cours de notre collaboration de deux ans, elle a toujours fait preuve d'une approche proactive, axée sur la recherche de solutions. Lucie sait s'adapter au changement, elle est rigoureuse dans l'exécution.",
+        },
+        {
+            picture: "frederic",
+            name: "Frédéric",
             company: "SINGULART",
-            review: "Sed ut perspiciatis unde, totam rem aperiam, eaque ipsa quaeab illo inventore veritatis et quasi architectobeatae vitae dicta sunt explicabo.",
-        },
-        {
-            picture: "sandrine",
-            name: "Sandrine",
-            company: "gîte La Souris des Champs",
-            review: "Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quaeab illo inventore veritatis et quasi architectobeatae vitae dicta sunt explicabo.",
+            review: "Techniquement compétente et extrêmement douée en communication, Lucie a constamment apporté des solutions créatives et des implémentations pertinentes à nos projets. Son enthousiasme et sa volonté d'apprendre sont contagieux, apportant une énergie positive et dynamisante à toute l'équipe.",
         },
     ];
 </script>
@@ -30,7 +30,11 @@
         >
             <img
                 class="testimonial__image"
-                src="https://picsum.photos/id/127/1920/1080"
+                :src="
+                    '/public/images/testimonials/' +
+                    testimonial.picture +
+                    '.jpg'
+                "
                 alt=""
             />
             <h2 class="testimonial__reviewer discrete">
